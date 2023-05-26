6d6bc26a43a575b04c0fa689f8dc09c507810118
Shuhui Luo <shuhui@aperture.finance>
2023-05-26 14:52:41 -0700

    Test `SwapMath`

--- src/test/SwapMathTest.sol
+++ src/test/SwapMathTest.sol
@@ -5,14 +5,14 @@ import "@uniswap/v3-core/contracts/libraries/SwapMath.sol";
 import "./interfaces/ISwapMath.sol";
 
 /// @dev Expose internal functions to test the SwapMath library.
-contract SwapMath is ISwapMath {
+contract SwapMathTest is ISwapMath {
     function computeSwapStep(
         uint160 sqrtRatioCurrentX96,
         uint160 sqrtRatioTargetX96,
         uint128 liquidity,
         int256 amountRemaining,
         uint24 feePips
-    ) external pure returns (uint160, uint256, uint256, uint256) {
+    ) external pure override returns (uint160, uint256, uint256, uint256) {
         return
             SwapMath.computeSwapStep(
                 sqrtRatioCurrentX96,
@@ -32,6 +32,7 @@ contract SwapMath is ISwapMath {
     )
         external
         pure
+        override
         returns (uint160 sqrtRatioNextX96, uint256 amountIn, uint256 amountOut)
     {
         (sqrtRatioNextX96, amountIn, amountOut, ) = SwapMath.computeSwapStep(
