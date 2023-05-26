cf283d032624aefdb9f6ef1575c9ff5ee66b18a3
Shuhui Luo <shuhui@aperture.finance>
2023-05-26 11:16:09 -0700

    Prepare to test `SwapMath`

---
+++ src/test/interfaces/ISwapMath.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+interface ISwapMath {
+    function computeSwapStep(
+        uint160 sqrtRatioCurrentX96,
+        uint160 sqrtRatioTargetX96,
+        uint128 liquidity,
+        int256 amountRemaining,
+        uint24 feePips
+    )
+        external
+        pure
+        returns (
+            uint160 sqrtRatioNextX96,
+            uint256 amountIn,
+            uint256 amountOut,
+            uint256 feeAmount
+        );
+
+    function computeSwapStepExactIn(
+        uint160 sqrtRatioCurrentX96,
+        uint160 sqrtRatioTargetX96,
+        uint128 liquidity,
+        uint256 amountRemaining,
+        uint256 feePips
+    )
+        external
+        pure
+        returns (uint160 sqrtRatioNextX96, uint256 amountIn, uint256 amountOut);
+}
