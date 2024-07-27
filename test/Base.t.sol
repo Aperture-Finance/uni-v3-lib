7a10500117ad5cbbc62b5bdaac76130f6c9a2876
gnarlycow <peiqianli@gmail.com>
2024-07-27 15:17:26 -0700

    Resolve comments.

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -12,7 +12,8 @@ import {TickBitmap} from "src/TickBitmap.sol";
 import {TickMath} from "src/TickMath.sol";
 import {ICommonNonfungiblePositionManager} from "src/interfaces/ICommonNonfungiblePositionManager.sol";
 
-// Partial interface for the SlipStream factory.
+// Partial interface for the SlipStream factory. SlipStream factory is named "CLFactory" and "CL" presumably stands for concentrated liquidity.
+// https://github.com/velodrome-finance/slipstream/blob/main/contracts/core/interfaces/ICLFactory.sol
 interface ISlipStreamCLFactory {
     /// @notice Returns the pool address for a given pair of tokens and a tick spacing, or address 0 if it does not exist
     /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
